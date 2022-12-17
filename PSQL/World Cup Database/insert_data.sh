838a990bbd8e1169691235cf63d9e43b8deffe49
Kylie <84819232+EvilDrPurple@users.noreply.github.com>
2022-12-16 20:34:13 -0500

    Update insert_data.sh
    Add comment and edit variable names for queries

--- PSQL/World Cup Database/insert_data.sh
+++ PSQL/World Cup Database/insert_data.sh
@@ -8,7 +8,9 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-Q=$($PSQL "TRUNCATE teams, games;")
+
+# truncate data
+TRUNCATE_TABLE=$($PSQL "TRUNCATE teams, games;")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
@@ -21,7 +23,7 @@ do
     if [[ -z $WINNER_ID ]]
     then
       # insert winner
-      Q=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
+      INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
 
       # get new winner_id
       WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
@@ -34,13 +36,13 @@ do
     if [[ -z $OPPONENT_ID ]]
     then
       # insert opponent
-      Q=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
 
       # get new opponent_id
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     fi
 
     # insert into games
-    Q=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    INSERT_GAMES=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
