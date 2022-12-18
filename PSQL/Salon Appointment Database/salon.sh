8aea68472a70e2bb46a222df985e9f72ef0de242
Kylie <84819232+EvilDrPurple@users.noreply.github.com>
2022-12-17 23:10:53 -0500

    Update salon.sh
    Add comments and change query variable names

--- PSQL/Salon Appointment Database/salon.sh
+++ PSQL/Salon Appointment Database/salon.sh
@@ -4,6 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
+# print service names
 PRINT_MENU() {
   if [[ $1 ]]
   then
@@ -24,35 +25,45 @@ PRINT_MENU
 
 read SERVICE_ID_SELECTED
 SERVICE_NAME=""
+# if user input is a number
 if [[ $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
 then
+  # get service name
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
 fi
 
+# while user input is invalid
 while [[ -z $SERVICE_NAME ]]
 do
   PRINT_MENU "I could not find that service. What would you like today?"
   read SERVICE_ID_SELECTED
+  # if user input is a number
   if [[ $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
   then
+    # get service name
     SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
   fi
 done
 
 echo -e "\nWhat's your phone number?"
 read CUSTOMER_PHONE
+# get customer name
 CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
 
+# if not found
 if [[ -z $CUSTOMER_NAME ]]
 then
   echo -e "\nI don't have a record for that phone number, what's your name?"
   read CUSTOMER_NAME
-  Q=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+  # insert new customer
+  INSERT_CUSTOMER=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
 fi
 
 echo -e "What time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
 read SERVICE_TIME
 
+# get customer id
 CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-Q=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+# insert appointment
+INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
 echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
