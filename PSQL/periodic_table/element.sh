94399cd68f922b545e1d6ce662f325a5f08c1044
Kylie <84819232+EvilDrPurple@users.noreply.github.com>
2022-12-20 23:41:45 -0500

    Update element.sh comments

--- PSQL/periodic_table/element.sh
+++ PSQL/periodic_table/element.sh
@@ -7,8 +7,10 @@ then
   # if search term is a number
   if [[ $1 =~ ^[0-9]+$ ]]
   then
+    # get element using atomic number
     ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number = $1")
   else
+    # get element using symbol or name
     ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE symbol = '$1' OR name = '$1'")
   fi
   
