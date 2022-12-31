5eeb0838676f493797ac37ab1c4fef4827077808
Kylie <84819232+EvilDrPurple@users.noreply.github.com>
2022-12-30 19:31:37 -0500

    Add comments to caesers-cipher.js

--- JavaScript/caesers-cipher.js
+++ JavaScript/caesers-cipher.js
@@ -1,13 +1,17 @@
 function rot13(str) {
   str = str.split('');
   str = str.map(function(letter) {
+    // if non-alphanumeric or whitespace
     if (/\W|\s/.test(letter)) return letter;
 
     let charCode = letter.charCodeAt(0);
     let shift = 13;
+    // difference between last char code and current char code
     const diff = 90 - charCode;
-
+    
+    // if difference is less than the shift
     if (diff < 13) {
+      // shift char code to the beginning
       shift -= diff;
       charCode = 64;
     }
@@ -17,7 +21,6 @@ function rot13(str) {
   });
 
   str = str.join('');
-  //console.log(str)
   return str;
 }
 
